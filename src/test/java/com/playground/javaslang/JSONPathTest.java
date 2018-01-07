be0e105c386d75d47516a6a311bf0e14683d7f06
Chinmay Chandragiri <cschandragiri@gmail.com>
2018-01-07 13:56:09 -0800

    jsonPath examples

--- src/test/java/com/playground/javaslang/JSONPathTest.java
+++ src/test/java/com/playground/javaslang/JSONPathTest.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.junit.Before;
@@ -15,6 +16,9 @@ import org.junit.Test;
 
 import com.jayway.jsonpath.Filter;
 import com.jayway.jsonpath.JsonPath;
+
+import io.vavr.Tuple;
+import net.minidev.json.JSONArray;
 
 public class JSONPathTest
 {
@@ -29,16 +33,25 @@ public class JSONPathTest
 				.collect(Collectors.joining());
 	}
 
-	Filter cheapFictionFilterAndNonFiction = filter(where("category").in("non-fiction", "fiction")
-			.and("price")
-			.lte(10D));
-
-	Filter thrillerOrWrittenByRevati = filter(where("author").eq("Revati B"));
-
 	@Test
-	public void test()
+	public void testFilter()
 	{
-		List<String> authors = JsonPath.read(jsonText, "$.store.book[*].author");
+		/**
+		 * Predicates
+		 */
+		Filter cheapFictionFilterAndNonFiction = filter(
+				where("category").in("non-fiction", "fiction")
+						.and("price")
+						.lte(10D));
+
+		Filter thrillerOrWrittenByRevati = filter(where("author").eq("Revati B"));
+
+		Filter titleShouldExist = filter(where("title").exists(true)
+				.and("title")
+				.ne(null));
+
+		/** Test cases ***/
+		JSONArray authors = JsonPath.read(jsonText, "$.store.book[*].author");
 		assertEquals(7, authors.size());
 
 		authors = JsonPath.read(jsonText, "$.store.book[?(@.price > 10)]");
@@ -48,8 +61,55 @@ public class JSONPathTest
 				cheapFictionFilterAndNonFiction);
 		assertEquals(2, authors.size());
 
-		List<String> books = JsonPath.read(jsonText, "$.store.book[?]",
+		JSONArray books = JsonPath.read(jsonText, "$.store.book[?]",
 				cheapFictionFilterAndNonFiction.or(thrillerOrWrittenByRevati));
 		assertEquals(3, books.size());
+
+		JSONArray expensive = JsonPath.parse(jsonText)
+				.read("$..['book'][?]", context ->
+				{
+					String value = context.item(Map.class)
+							.get("price")
+							.toString();
+					return Float.valueOf(value) > 20.00;
+				});
+		assertEquals(2, expensive.size());
+
+		JSONArray booksWithTitle = JsonPath.read(jsonText, "$.store.book[?]", titleShouldExist);
+		assertEquals(6, booksWithTitle.size());
+
+		JSONArray audioBooks = JsonPath.read(jsonText, "$..book[?]", context ->
+		{
+			@SuppressWarnings("unchecked")
+			List<Map<String, Object>> formats = (List<Map<String, Object>>) context.item(Map.class)
+					.get("format");
+			return formats.stream()
+					.map(entry -> Tuple.of(entry.getOrDefault("type", ""),
+							entry.getOrDefault("availableCopies", 0)))
+					.filter(tuple -> tuple._1.equals("audio-book") && (int) tuple._2 > 0)
+					.findAny()
+					.isPresent();
+		});
+		assertEquals(1, audioBooks.size());
+
+	}
+
+	@Test
+	public void testFunction()
+	{
+		/**
+		 * Predicates
+		 */
+		Filter cheapFictionFilterAndNonFiction = filter(
+				where("category").in("non-fiction", "fiction")
+						.and("price")
+						.lte(30D));
+
+		/** Avg ***/
+		JSONArray authors = JsonPath.read(jsonText, "$.store.book[?].price",
+				cheapFictionFilterAndNonFiction);
+		assertEquals(10.30, authors.stream().mapToDouble(e -> (Double)e).average().orElse(0), 0.5);
+		
+
 	}
 }
